6780d0f81ccad3db6234373346dd7623913ec592
Vigo Eka <vigoeka5@gmail.com>
2026-06-28 10:36:08 +0700

    Reworked player car movement

--- src/gameplay/player.c
+++ src/gameplay/player.c
@@ -8,10 +8,10 @@
 
 // Global constants
 static float
-	LINEAR_DAMPING 	= 2.5f,										// Between 2.0 ~ 8.0
+	LINEAR_DAMPING 	= 1.6f,										// Between 2.0 ~ 8.0
 	ANGULAR_DAMPING	= 14.0f,
 	DENSITY 		= 50.0f / (PX_PER_METER * PX_PER_METER),	// kg / m^2
-	TIRE_GRIP		= 0.85f,
+	TIRE_GRIP		= .85f,
 	STEERING_SPEED	= 600.0f;
 
 // Global variables
@@ -35,9 +35,8 @@ void InitPlayer(b2Vec2 initPos, float initAngle)
 {
 	// Set up initial car "body"
 	b2BodyDef bodyDef = b2DefaultBodyDef();
-	// TODO: uncomment this
-	// bodyDef.linearDamping = LINEAR_DAMPING;
-	// bodyDef.angularDamping = ANGULAR_DAMPING;
+	bodyDef.linearDamping = LINEAR_DAMPING;
+	bodyDef.angularDamping = ANGULAR_DAMPING;
 	bodyDef.position = initPos;
 	bodyDef.rotation = b2MakeRot(DEG2RAD * initAngle);
 	bodyDef.type = b2_dynamicBody;
@@ -71,7 +70,8 @@ void InitPlayer(b2Vec2 initPos, float initAngle)
 		car.bodyId,
 		(b2Vec2){ -carExtent.x , -carExtent.y }
 	);
-	carAngle = RAD2DEG * b2Rot_GetAngle(b2Body_GetRotation(car.bodyId));
+	carRad = b2Rot_GetAngle(bodyDef.rotation);
+	carAngle = RAD2DEG * carRad;
 
 	// Set initial car state
 	playerControl = 1;
@@ -83,30 +83,79 @@ void UpdatePlayer(float deltaTime)
 	float gasPad = (AccelerateInput() - BrakeInput()) * playerControl;
 	float steerPad = SteeringInput();
 
+	// Calculate engine force
 	float mass = b2Body_GetMass(car.bodyId);
-	float acceleration = 100.0f * PX_PER_METER * gasPad;
-	float force = mass * acceleration;
-	b2Body_ApplyForceToCenter(car.bodyId, (b2Vec2){ force, 0 }, true);
+	float acceleration = 50.0f * PX_PER_METER * gasPad;
+	float engineF = mass * acceleration;
+
+	// Set engine force appplication
+	b2Vec2 forcePoint = b2Add
+	(
+		carPosCenter,
+		b2RotateVector(b2Body_GetRotation(car.bodyId), (b2Vec2){ carExtent.x, .0f })
+	);
+
+	// Apply engine force
+	b2Vec2 force = { -(engineF), .0f };
+	b2Body_ApplyForce
+	(
+		car.bodyId,
+		b2RotateVector(b2Body_GetRotation(car.bodyId), force),
+		forcePoint,
+		true
+	);
+
+	// Calculate steering force
+	float steerF = .0f;
+	if (b2Length(b2Body_GetLinearVelocity(car.bodyId)) > PX_PER_METER * .3f)
+	{
+		steerF = mass * 40.0f * PX_PER_METER * steerPad;
+	}
+
+	// Set Steering force point
+	forcePoint = b2Add
+	(
+		carPosCenter,
+		b2RotateVector(b2Body_GetRotation(car.bodyId), (b2Vec2){ -carExtent.x, .0f })
+	);
+
+	// Apply steering force
+	force = (b2Vec2){ .0f, -(steerF) };
+	b2Body_ApplyForce
+	(
+		car.bodyId,
+		b2RotateVector(b2Body_GetRotation(car.bodyId), force),
+		forcePoint,
+		true
+	);
+
+	// Fix car's slowing down behaviour
+	b2Vec2 vel = b2Body_GetLinearVelocity(car.bodyId);
+	if (b2Length(vel) <= PX_PER_METER * .3f && FloatEquals(gasPad, .0f))
+	{
+		b2Body_SetLinearVelocity(car.bodyId, (b2Vec2){ .0f, .0f });
+	}
 
 	// DEBUG
-	// printf("mass: %.2f\n", mass);
-
-	// NOTE: uncomment these
-	// // Fix right velocity to control sliding
-	// Vector2 currentVelocity = B2vecToRlvec(b2Body_GetLinearVelocity(car.bodyId));
-	// Vector2 right =
+	// static float counter = 0;
+	// counter += GetFrameTime();
+	// if (counter > .5f)
 	// {
-	// 	cosf((carAngle + 90.0f) * DEG2RAD),
-	// 	sinf((carAngle + 90.0f) * DEG2RAD)
-	// };
-	// float lateralSpeed = Vector2DotProduct(currentVelocity, right);
-	// Vector2 killLateral = Vector2Scale(right, -lateralSpeed * TIRE_GRIP);
-	// b2Body_SetLinearVelocity
-	// (
-	// 	car.bodyId,
-	// 	RlvecToB2vec(Vector2Add(currentVelocity, killLateral))
-	// );
-	//
+	// 	counter = 0;
+	// 	printf("velocity: %.2f\n", b2Length(vel));
+	// }
+
+	// Fix right velocity to control sliding
+	Vector2 currentVelocity = B2vecToRlvec(b2Body_GetLinearVelocity(car.bodyId));
+	Vector2 right = { cosf(carRad + B2_PI * .5f), sinf(carRad + B2_PI * .5f) };
+	float lateralSpeed = Vector2DotProduct(currentVelocity, right);
+	Vector2 killLateral = Vector2Scale(right, -lateralSpeed * TIRE_GRIP);
+	b2Body_SetLinearVelocity
+	(
+		car.bodyId,
+		RlvecToB2vec(Vector2Add(currentVelocity, killLateral))
+	);
+
 	// Get linear and angular tranformations
 	carPosCenter = b2Body_GetWorldPoint(car.bodyId, (b2Vec2){ 0.0f, 0.0f });
 	carPosEdge = b2Body_GetWorldPoint
@@ -116,7 +165,8 @@ void UpdatePlayer(float deltaTime)
 	);
 	tireDest.x = carPosEdge.x;
 	tireDest.y = carPosEdge.y;
-	carAngle = RAD2DEG * b2Rot_GetAngle(b2Body_GetRotation(car.bodyId));
+	carRad = b2Rot_GetAngle(b2Body_GetRotation(car.bodyId));
+	carAngle = RAD2DEG * carRad;
 }
 
 void DrawPlayer(void)
